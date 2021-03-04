f73312b472f8ea50ca3590c0d998d2165cd6aafd
virgilxw <reubenwxw@gmail.com>
2021-03-04 16:31:57 +0800

    added accordian sidebar, site populates sidebar list from .csv, greys out and disables missing layers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,71 @@
+function populateSidebar(data) {
+	// As name implies
+
+	// Instantiate buffer lists
+	var list_alpha = [],
+		list_bravo = [],
+		list_charlie = []
+
+	//Iterate through data, a list of dictionaries to generate categorised lists
+	data.forEach(function (line) {
+		// Categorise line by category
+		if (line["Category"] == "A") {
+			list_alpha.push(line)
+		} else if (line["Category"] == "B") {
+			list_bravo.push(line)
+		} else if (line["Category"] == "C") {
+			list_charlie.push(line)
+		}
+	})
+
+	// Injecting lists into the right section
+
+	// Sorting out alpha
+	// Empty list, then re-inject new list
+	$("#layers_A > .list-group").empty()
+	list_alpha.forEach(function (line) {
+		var alpha_html_string = "<li class='list-group-item " + line["Code"] + "_li'><input type='checkbox' id='" + line["Code"] + "_checkbox'><span id='" + line["Code"] + "_text'>" + line["Code"] + ":" + line["Map Name"] + "</span><br><input type='range' class='form-range' id='" + line["Code"] + "_range'></li>"
+		$("#layers_A > .list-group").append(alpha_html_string)
+
+		// Helper function to link layers to points
+		link(line["Code"])
+	})
+
+	// Sorting out bravo
+	// Empty list, then re-inject new list
+	$("#layers_B > .list-group").empty()
+	list_bravo.forEach(function (line) {
+		var bravo_html_string = "<li class='list-group-item " + line["Code"] + "_li'><input type='checkbox' id='" + line["Code"] + "_checkbox'><span id='" + line["Code"] + "_text'>" + line["Code"] + ":" + line["Map Name"] + "</span><br><input type='range' class='form-range' id='" + line["Code"] + "_range'></li>"
+		$("#layers_B > .list-group").append(bravo_html_string)
+
+		// Helper function to link layers to points
+		link(line["Code"])
+	})
+
+	// Sorting out charlie
+	// Empty list, then re-inject new list
+	$("#layers_C > .list-group").empty()
+	list_charlie.forEach(function (line) {
+		var charlie_html_string = "<li class='list-group-item " + line["Code"] + "_li'><input type='checkbox' id='" + line["Code"] + "_checkbox'><span id='" + line["Code"] + "_text'>" + line["Code"] + ":" + line["Map Name"] + "</span><br><input type='range' class='form-range' id='" + line["Code"] + "_range'></li>"
+		$("#layers_C > .list-group").append(charlie_html_string)
+
+		// Helper function to link layers to points
+		link(line["Code"])
+	})
+}
+
+function link(code) {
+
+	var li_class_tag = "." + code + "_li"
+
+	// Verify the layer exists in Mapbox
+	var mapLayer = map.getLayer(code);
+	if (typeof mapLayer !== 'undefined') {} else {
+		$(li_class_tag).addClass("disabled")
+	}
+}
+
+// Init Map
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2hnaXMta2VubmV0aGRlYW4iLCJhIjoiY2tqMTBpOHl0MDI0YzJ5c2IzOHMyM2V4eCJ9.DFNMWEGdVJkBh9mS2OkrbA';
 var map = new mapboxgl.Map({
 	container: 'map',
@@ -6,70 +74,16 @@ var map = new mapboxgl.Map({
 	center: [103.8198, 1.3521]
 });
 
-map.on('load', function () {
+$(document).ready(function () {
 
-	// enumerate ids of the layers
-	var toggleableLayerIds = ["000 Alfred ER 1968", "000 INTERESTING large-scale-topographical-map-of-singapore-1945", "000 INTERESTING Singapore_1994_CIA_map", "000 interest LAND USE large-old-map-of-singapore-1898", "000 singapore1944", "00 MAYBE INFRASTR Dispositions_of_the_Garrison_February-1942_Singapore", "1983 Singapore p. 265_updated", "1983 Singapore p. 262_updated", "1983 Singapore p. 263_updated", "1976 W-K p. 129_updated"];
-
-	// set up the corresponding toggle button for each layer
-	for (var i = 0; i < toggleableLayerIds.length; i++) {
-
-		var id = toggleableLayerIds[i];
-
-
-
-		var htmlstring = "<input type='checkbox' class='checkbox' id='" + id + "' checked> <li> " + id + " </li><div class='slidecontainer'><input type='range' min='1' max='100' value='100' class='slider' id='" + id + "'</div > "
-
-		$("#menu").append(htmlstring)
-	}
-
-	$(".checkbox").change(function () {
-
-		id = $(this).attr('id')
-
-		if (this.checked) {
-			map.setLayoutProperty(id, 'visibility', 'visible');
-		} else {
-			map.setLayoutProperty(id, 'visibility', 'none');
+	var data;
+	$.ajax({
+		type: "GET",
+		url: "./Ledger_of_maps.csv",
+		dataType: "text",
+		success: function (response) {
+			data = $.csv.toObjects(response)
+			populateSidebar(data)
 		}
-
 	})
-
-	$(".slider").change(function () {
-		value = $(this)[0].value
-
-		map.setPaintProperty(
-			id,
-			'raster-opacity',
-			parseInt(value, 10) / 100
-		);
-	})
-})
-
-
-/**
-var link = document.createElement('a');
-link.href = '#';
-link.className = 'active';
-link.textContent = id;
-
-link.onclick = function (e) {
-	var clickedLayer = this.textContent;
-	e.preventDefault();
-	e.stopPropagation();
-
-	var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
-
-	// toggle layer visibility by changing the layout object's visibility property
-	if (visibility === 'visible') {
-		map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-		this.className = '';
-	} else {
-		this.className = 'active';
-		map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
-	}
-};
-
-var layers = document.getElementById('menu');
-layers.appendChild(link);
-**/
+});
---
+++ readme.md
@@ -0,0 +1,25 @@
+/* Color Theme Swatches in Hex */
+.Travel-1-hex { color: #0468BF; }
+.Travel-2-hex { color: #049DBF; }
+.Travel-3-hex { color: #3b592d; }
+.Travel-4-hex { color: #D99E6A; }
+.Travel-5-hex { color: #8C4332; }
+
+/* Color Theme Swatches in RGBA */
+.Travel-1-rgba { color: rgba(3, 103, 191, 1); }
+.Travel-2-rgba { color: rgba(3, 156, 191, 1); }
+.Travel-3-rgba { color: rgba(58, 89, 44, 1); }
+.Travel-4-rgba { color: rgba(216, 157, 106, 1); }
+.Travel-5-rgba { color: rgba(140, 66, 50, 1); }
+
+/* Color Theme Swatches in HSLA */
+.Travel-1-hsla { color: hsla(208, 96, 38, 1); }
+.Travel-2-hsla { color: hsla(191, 96, 38, 1); }
+.Travel-3-hsla { color: hsla(101, 33, 26, 1); }
+.Travel-4-hsla { color: hsla(28, 59, 63, 1); }
+.Travel-5-hsla { color: hsla(11, 47, 37, 1); }
+
+
+Credits:
+UneekGrafix, "Leaf icon 15.svg", https://en.wikipedia.org/wiki/File:Leaf_icon_15.svg
+Evanplace, typeiii/jquery-csv github: "https://github.com/typeiii/jquery-csv"
